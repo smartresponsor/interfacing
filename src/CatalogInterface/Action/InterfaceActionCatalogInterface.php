f948fead2528c4cb0eb7877d086f94d55b38f1a3
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-04 19:34:01 -0500

    Fix InterfaceActionCatalogInterface import

--- src/CatalogInterface/Action/InterfaceActionCatalogInterface.php
+++ src/CatalogInterface/Action/InterfaceActionCatalogInterface.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Interfacing\CatalogInterface\Action;
 
 use App\Interfacing\Contract\ValueObject\InterfaceActionIdInterface;
+use App\Interfacing\EndpointInterface\Action\InterfaceActionEndpointInterface;
 
 interface InterfaceActionCatalogInterface
 {
